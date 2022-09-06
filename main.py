36999d8ac3a5ac7420f16a1d87c44e57f7a7b750
ProgRiver <barkas.ssr@mail.ru>
2022-09-06 20:56:09 +0300

    разбор HTML

--- main.py
+++ main.py
@@ -6,7 +6,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>О сайте</h1>"
+    # return "<h1 align='center'><i>Information </i>О сайте</h1>"
+
+    # target='_blank' - ссылка открывается в новой вкладке
+    # return "<a href='https://github.com/ProgRiver' target='_blank'>My GitHub</a>"
+
+    # return "<img src='https://python.org/static/img/python-logo.png' width='580' height='170'>"
+
+    # return """<p align='center'>
+    #             <img src='https://python.org/static/img/python-logo.png' width='580' height='170'>
+    #           </p>"""
+
+    # изображения в качестве ссылок
+    return """<a href='https://python.org/' >
+                <img src='https://python.org/static/img/python-logo.png' width='580' height='170'>
+              </a>""" 
+
 
 @app.route('/about')
 def about():
