61d9dd4ca109fcfa06d57c9de786a8e190a67dec
ProgRiver <barkas.ssr@mail.ru>
2022-09-08 20:51:24 +0300

    передача переменных

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
+from random import choice
 
 app = Flask(__name__)
 
+number = 150
+# user_name = choice(("name-1", "name-2", "name-3", "name-4"))
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    user_name = choice(("name-1", "name-2", "name-3", "name-4"))
+    return render_template("index.html",
+                           name=user_name,
+                           project_number=number,
+                           title="Home page")
 
 
 if __name__ == "__main__":
